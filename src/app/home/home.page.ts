f387566e49c0dea2103a03027f0709f54dd9ae3c
Mathias Johannsen <m.johannsen.privat@gmail.com>
2022-09-09 16:28:29 +0200

    Created Node API call

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,42 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  api = 'https://overpass-api.de/api/interpreter?data=';
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  ngOnInit(): void {
+
+  }
+
+  test() {
+    console.log('hola');
+    this.getNodes(
+      '"amenity"="toilets"',
+      '47.44027965714996',
+      '9.673805236816404',
+      '47.517200697839414',
+      '9.818687438964844'
+    ).subscribe((data) => {
+      console.log('Data is here: ');
+
+      console.log(data);
+
+    });
+  }
+
+
+
+  getNodes(nodeType: string, lat1, lon1, lat2, lon2, timeout = 10, out = 'json'): Observable<any> {
+    const query =
+      '[bbox:' + lat1 + ',' + lon1 + ',' + lat2 + ',' + lon2 + ']' +
+      '[out:' + out + ']' +
+      '[timeout:' + timeout + '];' +
+      'node[' + nodeType + '];' +
+      'out;';
+    return this.http.get<any>(this.api + encodeURIComponent(query));
+  }
 
 }
