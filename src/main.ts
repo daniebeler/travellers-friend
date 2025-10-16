f7981ba276e42c89f253cc19dd86d775cf58714a
Daniel Hiebeler <hiebeler.daniel@gmail.com>
2025-10-16 17:42:42 +0200

    Add plausible

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { platformBrowser } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
+
+  const script = document.createElement('script');
+  script.defer = true;
+  script.setAttribute('data-domain', 'travellers-friend.netlify.app');
+  script.src = 'https://plausible.ghostbyte.dev/js/script.js';
+  document.head.appendChild(script);
 }
 
 platformBrowser().bootstrapModule(AppModule)
